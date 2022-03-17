3204aeeb2cb4b284fc0ca4a948fb2892fa73fe69
AlexanderPkv <OoLAVA2281337@mail.ru>
2022-03-17 18:30:58 +0300

    Added Task 07-generics

--- 07-generics/Generics/Task1/DynamicArray.cs
+++ 07-generics/Generics/Task1/DynamicArray.cs
@@ -11,9 +11,9 @@ namespace Task1
         /// <summary>
         /// Gets the total number of the elements contained in the array
         /// </summary>
-        public int Lenght 
+        public int Length
         {
-            get => data.Where(x => !x.Equals(default(T))).Count();                   
+            get => data.Where(x => !x.Equals(default(T))).Count();
         }
 
         /// <summary>
@@ -42,28 +42,85 @@ namespace Task1
         }
         #endregion
 
+        /// <summary>
+        /// Adds a new item to the end of the list
+        /// </summary>
+        /// <param name="item"></param>
         public void Add(T item)
         {
-            while(Lenght >= Capacity)
+            while (Length >= Capacity)
             {
                 Array.Resize(ref data, Capacity * 2);
             }
-            data[Lenght] = item;
+            data[Length] = item;
         }
 
+        /// <summary>
+        /// Adds range of the items to the end of the list
+        /// </summary>
+        /// <param name="items"></param>
         public void AddRange(T[] items)
         {
-            if (items.Length > Capacity)
+            if (items.Length + Length > Capacity)
             {
-                Array.Resize(ref data, Capacity + items.Length);
+                Array.Resize(ref data, (items.Length + Length) * 2);
             }
+            Array.Copy(items, 0, data, Length, items.Length);
         }
 
+        /// <summary>
+        /// Removes the item from collection
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>True if removing was successful, otherwise false</returns>
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                if (data[i].Equals(item))
+                {
+                    for (int j = i; j < Length - 1; j++)
+                    {
+                        data[j] = data[j + 1];
+                    }
 
+                    data[Length - 1] = default(T);
+                    return true;
+                }
+            }
+            return false;
+        }
 
+        /// <summary>
+        /// Inserts the item into the index position
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="index"></param>
+        public void Insert(T item, int index)
+        {
+            if (index > Length)
+            {
+                throw new ArgumentOutOfRangeException("Index is out of the range");
+            }
 
+            if (Capacity == Length)
+            {
+                Array.Resize(ref data, Capacity * 2);
+            }         
+         
+            for (int i = Length; i >= index; i--)
+            {
+                data[i + 1] = data[i];
+            }
 
+            data[index] = item;
+        }
 
+        public T this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
 
     }
 }
--- 07-generics/Generics/Task1/Program.cs
+++ 07-generics/Generics/Task1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Task1
 {
@@ -8,14 +7,17 @@ namespace Task1
         static void Main(string[] args)
         {
 
-            DynamicArray<int> list = new DynamicArray<int>(2);
+            DynamicArray<int> list = new DynamicArray<int>(1,2,3,4,5,6,7,8);
 
             list.Add(1);
-            list.Add(2);
-            list.Add(1);
-            list.Add(2); list.Add(1);
-            list.Add(2);
 
+            list.Insert(0,0);
+            list.Insert(1,8);
+
+            Console.WriteLine(list.Remove(0)); 
+
+            Console.WriteLine(list.Remove(66)); 
+           
 
 
 
