344b729e3526115c58d1fd97a30eb839542c6280
AlexanderPkv <OoLAVA2281337@mail.ru>
2022-04-04 10:42:05 +0300

    Added task11_beta

---
+++ 11-files/Files/Task2/Audit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Task2
+{
+    internal class Audit
+    {
+        private FileSystemWatcher watcher;
+        private string path;
+
+        public Audit(string path)
+        {
+            this.path = path;
+        }
+
+        public void StartWork()
+        {
+            watcher = new FileSystemWatcher(path);
+
+            watcher.Changed += FileModified;
+            watcher.Created += FileModified;
+            watcher.Deleted += FileModified;
+            watcher.Renamed += FileModified;
+
+            watcher.Filter = "*.txt";
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void FileModified(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.FullPath} has been changed");
+        }
+    }
+}
---
+++ 11-files/Files/Task2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+
+namespace Task2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AuditModeStart();
+
+            //StartProgram();
+           
+        }
+
+        static void StartProgram()
+        {
+
+            Console.WriteLine("Choose mode: 1. AuditMode 2. RollingBackMode");
+
+            string workMode;
+            do
+            {
+                workMode = Console.ReadLine().Trim();
+
+                switch (workMode)
+                {
+                    case "AuditMode":
+                        AuditModeStart();
+                        break;
+                    case "RollingBackMode":
+                        RollingBackModeStart();
+                        break;
+                    case "q":
+                        Console.WriteLine("Stopping the application...");
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect WorkMode");
+                        break;
+                }
+
+            } while (workMode != "q");
+        }
+
+       
+            
+       
+
+        static void AuditModeStart()
+        {
+            Console.WriteLine("Current mode: AuditMode");
+
+
+            Audit audit = new Audit("github");
+            audit.StartWork();
+           
+
+
+            while (Console.ReadLine() != "q") ;
+        
+
+        }
+
+        static void RollingBackModeStart()
+        {
+            Console.WriteLine("Current mode: RollingBackMode");
+
+            string dateToRecover;
+
+            do
+            {
+                dateToRecover = Console.ReadLine().Trim();
+
+                if (DateTime.TryParse(dateToRecover,out DateTime dateTime))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect DateTime");
+                }
+
+
+
+            } while (dateToRecover != "q");
+        }
+    }
+}
